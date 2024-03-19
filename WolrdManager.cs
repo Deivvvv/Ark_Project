c598e476fcad85d60a9c0fa04ec77b92fd7d4bb5
Deivvvv <52557350+Deivvvv@users.noreply.github.com>
2024-03-19 13:34:07 +0500

    Add

--- WolrdManager.cs
+++ WolrdManager.cs
@@ -10,9 +10,11 @@ class Chunk
     public Vector2Int Position;
     public string Tayp;
 
-    public Chunk(int x, int y , string Tayp)
+    public Chunk(Vector2Int x, string tayp)
     {
-        Position = new Vector2Int(x, y);
+        Position = x;
+        Tayp = tayp;
+
     }
 }
 public class WolrdManager : MonoBehaviour
@@ -31,13 +33,59 @@ public class WolrdManager : MonoBehaviour
         FindPath(startPoint, endPoint);
 
     }
+    Chunk GetChunk(Vector2Int v,int i)
+    {
+        switch (i)
+        {
+            case (0):
+                v += new Vector2Int(0, 1);
+                break;
+            case (1):
+                v += new Vector2Int(1, 0);
+                break;
+            case (2):
+                v += new Vector2Int(0, -1);
+                break;
+            case (3):
+                v += new Vector2Int(-1, 0);
+                break;
+        }
+
+
+        i = worldCell.FindIndex(x => x.Position == v);
+        if (i != -1)
+            return worldCell[i];
+
+        return AddChunk(v);
+    }
+    Chunk AddChunk( Vector2Int v)
+    {
+        Chunk chunk = new Chunk(v, "Block");
+        worldCell.Add(chunk);
+        return chunk;
+    }
 
     void FindPath(Vector2Int x, Vrctor2Int y)
     {
-
+        
     }
 }
 
+class PathCaseMain
+{
+    public int Id;
+    public string Tayp;
+    public Vector2Int Position;
+    public int cor;//x,y,-x,-y
+    public int Length;
+
+}
+
+
+class IndysryPath
+{
+
+}
 
 
 
